c2dc0a5fafcec66bb06460bfafffbb8f56ffc499
supermaro84 <mpierzchala84@gmail.com>
2018-05-02 11:33:34 +0200

    subscribe to gps

--- scripts/image_trigger.py
+++ scripts/image_trigger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+from rosflight_msgs import GPS
 
 
 def callback(data):
@@ -8,14 +9,10 @@ def callback(data):
 
 
 def listener():
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
+
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("chatter", String, callback)
+    rospy.Subscriber("/gps", GPS, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
