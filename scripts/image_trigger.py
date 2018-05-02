e933e9564e9dc796b740d3a1ce1317a2f75476c4
supermaro84 <mpierzchala84@gmail.com>
2018-05-02 11:55:08 +0200

    subscribe to gps

--- scripts/image_trigger.py
+++ scripts/image_trigger.py
@@ -2,11 +2,15 @@
 import rospy
 from std_msgs.msg import String
 from rosflight_msgs.msg import GPS
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
 
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.latitude)
-
+    camera.capture('%s.jpg'%(data.latitude))
 
 def listener():
 
